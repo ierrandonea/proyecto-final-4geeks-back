c265a0c91ecabc1e60a456ac2622b810fa7efb8d
ierrandonea <ierrandoneag@gmail.com>
2020-09-15 11:32:22 -0300

    added link to database schema for consultation

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ import json
 from flask_sqlalchemy import SQLAlchemy
 db=SQLAlchemy()
 
+# to consult database schema: https://dbdesigner.page.link/xvnBfzE4JMMCnLmx5
+
 class Product(db.Model):
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
